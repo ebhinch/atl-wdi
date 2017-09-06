b9d5b95689099654990418234f8d6b9a5e32d881
ebhinch <erica.hinchman@gmail.com>
2017-09-06 19:49:59 -0400

    progress bar homework complete.

--- homework/javaScript/progress-bar/hw.js
+++ homework/javaScript/progress-bar/hw.js
@@ -3,16 +3,78 @@
 // See LICENSE for details.
 
 var timerUI = {
+  // 1. The Numeric Display
+  // Your first priority will be to show the current value left on the clock. Every time the countdown value updates, the numeric display will need to update as well. The function you'll be writing to implement this is drawNumericDisplay(timerValue).
+
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+    // set numeric display to current value of timer
+    let numericDisplay = document.getElementById ("numeric-display");
+
+    // assign timerValue
+    numericDisplay.textContent = timerValue;
+    
+    // at 10 seconds, change color to red, increase size by 5% per second, round to 2 decimals
+    if (timerValue <= 10) {
+      numericDisplay.style.color = "red";
+
+      // increase font size by 5% every second starting when switches to 10 and round to 2 decimals 
+      let newSize = (1.5 * Math.pow(1.05, 11-timerValue)).toFixed(2);
+
+      // tell code to use new size
+      numericDisplay.style.fontSize = newSize + "em";
+    }
   },
+
   drawProgressBars: function(timerValue){
-    // Your Code Here
+    // 2. The Progress Bar
+    // The progress bar is a rectangle (with class progress-bar) inside the element with id progress-bars. As the timer counts down from 100 to 0, the progress bar should expand its width from 0% to 100% of the width of its parent element, the progress-bars component. The function responsible for implementing this is drawProgressBars(timerValue).
+  
+    // calculate how much time has elapsed
+    let timeElapsed = 100 - timerValue;
+    // set width to change as time elapses
+    document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + "%";
   },
+
   drawLitFuses: function(timerValue){
-    // Your Code Here
+    // 3. The Lit Fuse
+    // This one's a little cutesy. Similar to to the progress bar, the lengths of the .burnt and .unburnt divs will need to change over time as the counter runs down. Specifically. .unburnt will need to go from 98% of the width of the component (since the .flame div also takes up some space) to 0% of the width as the timer runs down, while .burnt does the reverse. The function responsible for implementing this is drawLitFuses(timerValue).
+
+
+    // calculate unburnt percent
+    let unburntPercent = timerValue;
+
+    // calculate burnt percent
+    let burntPercent = 100 - unburntPercent;
+
+    // tell "burnt" portion to increase in width
+    document.getElementsByClassName('burnt')[0].style.width = burntPercent*.98 + "%";
+
+    // tell "unburnt" portion to decrease in width
+    document.getElementsByClassName('unburnt')[0].style.width = unburntPercent*.98 + "%";
   },
+
+
   drawCrawlers: function(timerValue){
-    // Your Code Here
+    // 4. The Crawler
+    // If you thought the last one was cutesy, hoo boy. The crawler will make its way from left to right across the screen, at a rate of 10 pixels per second. Additionally, as the crawler makes its way across the screen, it should move back and forth (i.e. up and down) by 10 px with each step. The function responsible for implementing this behavior is drawCrawlers(timerValue).
+  
+    // calculate uncrawled percent
+    let uncrawledPercent = timerValue;
+ 
+    // calculate crawled percent
+    let crawledPercent = 100 - uncrawledPercent;
+
+    let timeLapsed = 100 - timerValue;
+
+    // tell crawler to move 10 pixels per second
+    document.getElementsByClassName('crawler')[0].style.marginLeft = timeLapsed * 10 + "px";
+
+    // if time elapsed is odd, move up and if time elapsed is even move down - both by 10px
+
+    if (timeLapsed % 2 === 0){
+      document.getElementsByClassName('crawler')[0].style.marginTop = 10 + "px";
+    }
+    else {document.getElementsByClassName('crawler')[0].style.marginTop = 0 + "px";
+  }
   }
 };
--- labs/javaScript/js-functions/functions-lab-starter.js
+++ labs/javaScript/js-functions/functions-lab-starter.js
@@ -1,60 +1,138 @@
 // Question 1
-function maxOfTwoNumbers() {
+// Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. Do some Googling to figure this out if you forget how conditionals work.
 
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 < num2) {
+        return num2;
+    }
+    else return num1;
 }
+console.log(maxOfTwoNumbers(7, 10));
 
 // Question 2
-function maxOfThree() {
+// Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
 
+function maxOfThree(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    }
+    else if (num2 > num1 && num2 > num3) {
+        return num2;
+    }
+    else return num3;
 }
+console.log(maxOfThree(2, 3, 1));
 
 // Question 3
-function isCharacterAVowel() {
+// Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel and false, otherwise.
 
+function isCharacterAVowel(letter) {
+    const lowerLetter = letter.toLowerCase();
+
+    if (lowerLetter == "a" || lowerLetter == "e" || lowerLetter == "i" || lowerLetter == "o" || lowerLetter == "u") {
+        return true;
+    }
+    else return false;
 }
+console.log(isCharacterAVowel("t"));
+console.log(isCharacterAVowel("T"));
+console.log(isCharacterAVowel("a"));
+console.log(isCharacterAVowel("A"));
 
 // Question 4
-function sumArray() {
- 
+// Define a function sumArray that sums all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10.
+
+const addUs = [];
+function sumArray(addUs) {
+    let sum = 0;
+    for (let i = 0; i<addUs.length; i++) {
+        sum += addUs[i];
+    }
+    return sum;
 }
+console.log(sumArray([1, 2, 3]));
 
 
 // Question 4
-function multiplyArray() {
+// Define a function multiplyArray that multiplies all the numbers in an array of numbers. For example, multiplyArray([1,2,3,4]) should return 24.
 
+const timesUs = [];
+function multiplyArray(timesUs) {
+    let product = 1;
+    for (let i = 0; i<timesUs.length; i++) {
+        product = product * timesUs[i];
+    }
+    return product;
 }
+console.log(multiplyArray([2, 4, 6]));
 
 
 // Question 5
-var numberOfArguments = function(){
-  
+// Write a function that returns the number of arguments passed to the function when called.
+
+var numberOfArguments = function(num){
+    const argumentCount = numberOfArguments.arguments.length;
+ 
+    return argumentCount;
 }
+console.log(numberOfArguments("hi", "there", "sally", "jones"));
 
 
 
 // Question 6
-var reverseString = function (){
-  
+// Define a function reverseString that reverses a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+
+var reverseString = function (string){
+    const splitString = string.split("");
+    const reversed = splitString.reverse().join("");
+  return reversed;
 };
+
+console.log(reverseString("erica"));
 
 
 // Question 7
-function findLongestWord () {
-  
-}
+// Write a function findLongestWord that takes an array of words and returns the length of the longest word in the array.
 
+const wordArray = [];
+let imLongest;
+
+function findLongestWord (wordArray) {
+    let longestWord = 0;
+  
+    for (let i = 0; i<wordArray.length; i++) {
+        if (wordArray[i].length > longestWord) {
+            longestWord = wordArray[i].length;
+            imLongest = wordArray[i];
+        }
+    }
+  return longestWord;
+  return imLongest;
+}
+console.log(findLongestWord(["blue", "orange", "periwinkle"]));
+console.log(imLongest);
 
 // Question 8
-function filterLongWords () {
-  
+// Write a function filterLongWords that takes an array of words and a number i and returns a new array of words that are longer than i characters long.
+
+
+const words = [];
+function filterLongWords (words) {
+   const i = 4;
+   let longWords = [];
+   for (let j = 0; j < words.length; j++) {
+       if (words[j].length > i) {
+           longWords.push(words[j]);
+       }
+   }
+ return longWords;
 }
+console.log(filterLongWords(["hello", "goodbye", "cya", "pineapple", "taco"]));
 
 
 // Bonus 1
-//??????
+// Attach the function reverseString (from question 6) to the object String so that it is possible to call: "General Assembly".reverseString().
 
 
 // Bonus 2
-function charactersOccurencesCount() {
-  
-}
+// function charactersOccurencesCount() {
