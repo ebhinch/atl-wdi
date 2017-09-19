f9df9a991811bb3573fa20050b5f26a2679cfe9e
ebhinch <erica.hinchman@gmail.com>
2017-09-19 17:28:31 -0400

    completed GET routes

--- homework/express/pizza-express/app.js
+++ homework/express/pizza-express/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    console.log(req.query);
+    res.send("Welcome to Pizza Experss!");
+})
+
+app.get("/topping/:type", (req, res, next) => {
+    const type = req.params.type;
+    res.send(`${type} pizza! Good choice.`)
+
+})
+
+app.get("/order/:amount/:size", (req, res, next) => {
+    const amount = req.params.amount;
+    const size = req.params.size;
+    res.send(`Your order for ${amount} ${size} pizzas will be ready in 1 minute!`);
+
+})
 
 
 
