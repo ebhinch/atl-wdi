dfe27f880e5933e4162b04965981cef20d65e190
ebhinch <erica.hinchman@gmail.com>
2017-09-23 15:12:04 -0400

    part 2: added arguments to Tamagotchi

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -2,7 +2,9 @@ console.log('tamagotchi file is loaded');
 
 //your class declaration here
 class Tamagotchi {
-   constructor() {
+   constructor(name, creatureType) {
+    this.name = name;
+    this.creatureType = creatureType;
 
     this.foodInTummy = 10;
     this.restedness = 10;
@@ -16,13 +18,17 @@ class Tamagotchi {
 
 }
 
-
 //create new Tamagotchis
-const tommy = new Tamagotchi();
-const sam = new Tamagotchi();
+// const tommy = new Tamagotchi();
+// const sam = new Tamagotchi();
 
-console.log(tommy.cry());
-console.log(sam.cry());
+// console.log(tommy.cry());
+// console.log(sam.cry());
+
+const fluffykins = new Tamagotchi("fluffykins", "bunny");
+const fido = new Tamagotchi("fido", "dog");
+console.log(fluffykins.cry());
+console.log(fido.cry());
 
 
 //test out your Tamagotchies below via console.logs
