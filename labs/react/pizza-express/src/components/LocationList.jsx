71f7614c23dc1da0cfd98e4509434de59f1530af
ebhinch <erica.hinchman@gmail.com>
2017-10-09 19:29:25 -0400

    uploading react pizza homework

---
+++ labs/react/pizza-express/src/components/LocationList.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import locationData from '../locationData.js'
+import Location from './Location.jsx'
+
+class LocationList extends Component{
+    render() {
+        const location = locationData.map((location, imdex) => {
+            return <Location
+            name={location.name}
+            address={location.address}
+            phone={location.phone}
+            />
+        })
+       
+
+
+        return(
+        <div id="locations"> 
+            <h1>Locations</h1> 
+            <div className="locations">
+            {location}
+            </div>
+        </div>
+        )
+    }
+}
+
+
+export default LocationList
