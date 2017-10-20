1ba8ab63acfe6bd83132c21b0ead48c4c52151bd
ebhinch <erica.hinchman@gmail.com>
2017-10-19 22:17:44 -0400

    Realty queries completed

--- homework/sql/realty/realty.sql
+++ homework/sql/realty/realty.sql
@@ -5,23 +5,26 @@ SELECT AVG (square_footage) FROM offices;
 SELECT COUNT (*) FROM apartments;
 
 -- apartments with no tenant
-SELECT * FROM apartments WHERE tenant = false;
+SELECT * FROM apartments WHERE occupied_status = false;
 
 -- names of all companies
-SELECT name FROM companies;
+SELECT company_name FROM offices;
 
 -- number of cubicles and bathrooms in office 2
-SELECT bathrooms. cubices FROM offices WHERE id=2;
+SELECT bathroom_count, cubicle_count FROM offices WHERE id=2;
 
 -- storefronts with kitchens
 SELECT * FROM storefronts WHERE kitchen = true;
 
 -- storefront with highest square footage and outdoor seating
-SELECT * FROM storefronts ORDER BY square_footage DESC LIMIT 1 WHERE outdoor_seating='true';
+;
+
+SELECT * FROM storefronts ORDER BY square_footage DESC LIMIT 1;
+
 
 -- office wth lowest number of cubicles 
-SELECT * FROM offices ORDER BY square_footage DESC LIMIT 1
+SELECT * FROM offices ORDER BY square_footage DESC LIMIT 1;
 
 -- office with most cubices and bathrooms
-SELECT * FROM offices ORDER BY cubicle_count AND bathroom_count DESC LIMIT 1
+SELECT * FROM offices ORDER BY cubicle_count + bathroom_count DESC LIMIT 1;
 
