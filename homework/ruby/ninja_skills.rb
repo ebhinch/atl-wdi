1fc43f1e0eda38fdd12ef1937fd9f1384d3c2e7e
ebhinch <erica.hinchman@gmail.com>
2017-10-23 17:15:11 -0400

    1. Boolean Skills

---
+++ homework/ruby/ninja_skills.rb
@@ -0,0 +1,10 @@
+# Challenge 1: Boolean
+# Create a variable a and set it equal to true
+a = true
+# Create a variable b and set it equal to false
+b = false
+# Write a statement using a and b that evaluates to false
+puts a == b 
+# Write a statement using a and b that evaluates to true
+puts b != a 
+
